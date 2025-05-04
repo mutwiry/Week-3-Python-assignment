86c7aefbae44e8b3a57212ada908a31291f72244
vincent-Mutwiri <Vincent@ihub.co.ke>
2025-05-04 20:31:55 +0300

    added answers to the assignment

--- index.py
+++ index.py
@@ -1,1 +1,14 @@
-"#week 3 assignment" 
+#week 3 assignment
+def calculate_discount(price, discount_percentage):
+
+
+    if discount_percentage >= 0.2:
+        price =(1-discount_percentage) * price
+    return price
+
+result = calculate_discount(200, 0.2)
+print(result) # 160.0
+# Get user input for price and discount percentage
+price = float(input("Enter the price of the item: "))
+discount_percentage = float(input("Enter the discount percentage (0-1): "))
+print("The final price after discount is: ", calculate_discount(price, discount_percentage))
